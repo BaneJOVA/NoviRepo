f41bc1e8348a9f94b0eb76926752b135aa758857
38164 <branislav.jovanovic@gmail.com>
2022-11-29 22:26:03 +0100

    Init

---
+++ NoviRepo/NoviRepo/WebDriver/WebPage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoviRepo.WebDriver
+{
+    internal class WebPage
+    {
+    }
+}
